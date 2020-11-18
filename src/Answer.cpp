fbc84bb64b82ca8dd8291e8ef903f7ad540d5c6c
itachoco <b1615566@planet.kanazawa-it.ac.jp>
2020-11-18 13:06:27 +0900

    fin

--- src/Answer.cpp
+++ src/Answer.cpp
@@ -11,15 +11,23 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <queue>
 #include <random>
 #include <cmath>
  
-#define INF 1000000000.0
+#define INF 1000000000000.0
  
 hpc::Vector2 target;
 double* distance_map = nullptr;
 std::vector<int> target_list;
 int list_index = 0;
+std::vector<std::vector<int>> all_map;
+std::vector<std::vector<int>> start_to_scroll;
+
+void map_initialize(double* map, int mapsize)
+{
+    for(int i = 0; i < mapsize; ++i) map[i] = INF;
+}
  
 //------------------------------------------------------------------------------
 namespace hpc {
@@ -38,10 +46,6 @@ Answer::~Answer()
 {
 }
 
-void map_initialize(double* map, int mapsize)
-{
-    for(int i = 0; i < mapsize; ++i) map[i] = INF;
-}
 
 void around_minimam(int x, int y, double* map, const Stage& aStage)
 {
@@ -253,6 +257,98 @@ void clockwise(const Stage& aStage, double* map, const int start_x, const int st
     }
 }
 
+std::vector<int> aster_func(int start_x, int start_y, int target_x, int target_y, double* f_map, double* n_map, const Stage& aStage)
+{
+    double add_point;
+    bool undone_flag = true;
+    std::deque<std::pair<int, double>> open_list;
+    std::vector<int> result;
+    bool open_flag[Parameter::StageHeight * Parameter::StageWidth];
+    int parent_list[Parameter::StageWidth * Parameter::StageHeight];
+    double astarmap[Parameter::StageWidth * Parameter::StageHeight];
+    for(int i = 0; i < Parameter::StageWidth * Parameter::StageHeight; ++i)
+    {
+        open_flag[i] = true;
+        parent_list[i] = -1;
+        astarmap[i] = 9.0 * INF;
+    }
+    int start_point = start_x + start_y * Parameter::StageWidth;
+    int goal_point = target_x + target_y * Parameter::StageWidth;
+    int now_point = start_point;
+    astarmap[now_point] = f_map[now_point] + n_map[now_point];
+    open_list.push_back(std::pair<int, double>(now_point, f_map[now_point] + n_map[now_point]));
+
+    while(undone_flag)
+    {
+        if(open_list.empty())
+        {
+            break;
+        }
+        now_point = open_list.front().first;
+        open_list.pop_front();
+        if(now_point == goal_point)
+        {
+            break;
+        }
+        if(!open_flag[now_point])continue;
+
+        
+        hpc::Terrain now_ground = aStage.terrain(Vector2(static_cast<float>(now_point % Parameter::StageWidth), static_cast<float>(now_point / Parameter::StageWidth)));
+        if(now_ground == hpc::Terrain::Plain)
+        {
+            add_point = 1.0;
+        }
+        else if(now_ground == hpc::Terrain::Bush)
+        {
+            add_point = 1.6;
+        }
+        else if(now_ground == hpc::Terrain::Sand)
+        {
+            add_point = 3.0;
+        }
+        else
+        {
+            add_point = 10.0;
+        }
+            
+        std::vector<std::pair<int, double>> list;
+        list.clear();
+        for(int i = -1; i <= 1; ++i)
+        {
+            int x = i + now_point % Parameter::StageWidth; 
+            if(x < 0 || x > 49) continue;
+            for(int j = -1; j <= 1; ++j)
+            {
+                double cost = (i == 0) || (j == 0) ? 1.0 : 1.4;
+                int y = j + now_point / Parameter::StageWidth;
+                if(y < 0 || y > 49) continue;
+                if((i == 0 && j == 0)) continue;
+                if(astarmap[now_point] - n_map[now_point] + add_point * cost + n_map[x + y * Parameter::StageWidth] < astarmap[x + y * Parameter::StageWidth])
+                {
+                    list.push_back(std::pair<int, double>(x + y * Parameter::StageWidth, astarmap[now_point] - n_map[now_point] + add_point * cost + n_map[x + y * Parameter::StageWidth]));
+                    astarmap[x + y * Parameter::StageWidth] = astarmap[now_point] - n_map[now_point] + add_point * cost + n_map[x + y * Parameter::StageWidth];
+                    open_flag[x + y * Parameter::StageWidth] = true;
+                    parent_list[x + (y) * Parameter::StageWidth] = now_point;
+                }
+            }
+        }
+        open_flag[now_point] = false;
+        
+        for(auto itr : list)
+        {
+            open_list.push_back(itr);
+        }
+        std::sort(open_list.begin(), open_list.end(), [](std::pair<int, double>a, std::pair<int, double>b){return a.second < b.second;});
+    }
+    now_point = goal_point;
+    while(now_point != start_point)
+    {
+        result.push_back(now_point);
+        now_point = parent_list[now_point];
+    }
+    return result;
+}
+
 void calc_distance(const Stage& aStage, double* map, int start_x, int start_y)
 {
     int x = start_x;
@@ -262,8 +358,8 @@ void calc_distance(const Stage& aStage, double* map, int start_x, int start_y)
     double* counter_clockwise_map = new double[Parameter::StageWidth * Parameter::StageHeight];
     map_initialize(clockwise_map, Parameter::StageHeight * Parameter::StageWidth);
     map_initialize(counter_clockwise_map, Parameter::StageWidth * Parameter::StageHeight);
-    clockwise_map[x + y * Parameter::StageWidth] = 1.0;
-    counter_clockwise_map[x + y * Parameter::StageWidth] = 1.0;
+    clockwise_map[x + y * Parameter::StageWidth] = 0.0;
+    counter_clockwise_map[x + y * Parameter::StageWidth] = 0.0;
     for(int i = 0; i < 2; ++i)
     {
         counter_clockwise(aStage, counter_clockwise_map, x, y);
@@ -285,35 +381,26 @@ void calc_distance(const Stage& aStage, double* map, int start_x, int start_y)
     delete[] counter_clockwise_map;
 }
 
-
-void create_distance_map(const Stage& aStage, int target_x, int target_y)
+void create_distance_map(const Stage& aStage, int target_x, int target_y, int start_x, int start_y, double* result_map)
 {
     int stage_length = Parameter::StageWidth * Parameter::StageHeight;
+    std::vector<int> tmp;
     double* map1 = new double[stage_length];
-    //double* map2 = new double[stage_length];
-    double max = 0.0;
-    int now_point = static_cast<int>(aStage.rabbit().pos().x) + static_cast<int>(aStage.rabbit().pos().y) * Parameter::StageWidth;
+    double* map2 = new double[stage_length];
     map_initialize(map1, stage_length);
-    //map_initialize(map2, stage_length);
+    map_initialize(map2, stage_length);
     calc_distance(aStage, map1, target_x, target_y);
-    //calc_distance(aStage, map2, static_cast<int>(aStage.rabbit().pos().x), static_cast<int>(aStage.rabbit().pos().y));
-    
-    max = map1[now_point];
-    
-    for(int i = 0; i < stage_length; ++i)
+    calc_distance(aStage, map2, static_cast<int>(aStage.rabbit().pos().x), static_cast<int>(aStage.rabbit().pos().y));
+
+    map_initialize(result_map, Parameter::StageHeight * Parameter::StageWidth);
+    tmp = aster_func(start_x, start_y, target_x, target_y, map2, map1, aStage);
+    for(int i = 0; i < tmp.size(); ++i)
     {
-        if(map1[now_point] <= max)
-        {
-            distance_map[i] = map1[i];
-        }
-        else
-        {
-            distance_map[i] = INF;
-        }
+        result_map[tmp[i]] = static_cast<double>(i);
     }
     
     delete[] map1;
-    //delete[] map2;
+    delete[] map2;
 }
  
 Vector2 setTarget(const Stage& aStage)
@@ -346,11 +433,8 @@ Vector2 setTarget(const Stage& aStage)
             min = tmp;
         }
     }
-    for(int i = 0; i < Parameter::StageWidth * Parameter::StageHeight; ++i)
-    {
-        distance_map[i] = INF;
-    }
-    create_distance_map(aStage, static_cast<int>(result.x), static_cast<int>(result.y));
+    
+    create_distance_map(aStage, static_cast<int>(result.x), static_cast<int>(result.y), static_cast<int>(aStage.rabbit().pos().x), static_cast<int>(aStage.rabbit().pos().y), distance_map);
     return result;
 }
  
@@ -385,9 +469,10 @@ std::vector<int> yakinamashi(const Stage& aStage, int points_count, double* aDis
         result.push_back(0);
         return result;
     }
+    
     double turn = 0.0;
     double temp = 0.0;
-    const double heat = 100.0;
+    const double heat = 10000.0;
     double before = 0.0;
     int before_state = 0;
     char flag[points_count];
@@ -433,10 +518,10 @@ std::vector<int> yakinamashi(const Stage& aStage, int points_count, double* aDis
     }
     result = point;
     before = turn;
-    for(temp = heat; temp >= 0.1; temp *= 0.99999)
+    for(temp = heat; temp >= 0.001; temp *= 0.9999)
     {
         turn = 0.0;
-        double diff = heat / 100.0;
+        double diff = heat / 1000.0;
         for(double T = temp; T >= 0.0; T -= diff)
         {
             int lh = rnd.randMinMax(0, points_count - 1);
@@ -483,21 +568,22 @@ std::vector<int> create_list(const Stage& aStage)
     std::vector<double> start_cost;
     std::vector<int> result;
     int scrolls_count = aStage.scrolls().count();
-    map_initialize(map, Parameter::StageWidth * Parameter::StageHeight);
-    calc_distance(aStage, map, static_cast<int>(aStage.rabbit().pos().x), static_cast<int>(aStage.rabbit().pos().y));
-    for(auto itr = aStage.scrolls().begin(); itr != aStage.scrolls().end(); ++itr)
+    for(int i = 0; i < Parameter::MaxScrollCount * Parameter::MaxScrollCount; ++i) lDistanceMap[i] = INF;
+    for(int i = 0; i < aStage.scrolls().count(); ++i)
     {
-        start_cost.push_back(map[static_cast<int>(itr->pos().x + static_cast<int>(itr->pos().y) * Parameter::StageWidth)]);
+        start_cost.push_back(start_to_scroll[i].size());
     } 
     for(int i = 0; i < aStage.scrolls().count(); ++i)
     {
-        Vector2 pos = aStage.scrolls()[i].pos();
-        map_initialize(map, Parameter::StageWidth * Parameter::StageHeight);
-        calc_distance(aStage, map, static_cast<int>(pos.x), static_cast<int>(pos.y));
         for(int j = 0; j < aStage.scrolls().count(); ++j)
         {
-            int position = static_cast<int>(aStage.scrolls()[j].pos().x) + static_cast<int>(aStage.scrolls()[j].pos().y) * Parameter::StageWidth;
-            lDistanceMap[i * Parameter::MaxScrollCount + j] = map[position];
+            if(i == j) continue;
+            map_initialize(map, Parameter::StageHeight * Parameter::StageWidth);
+            for(int k = 0; k < all_map[j + i * aStage.scrolls().count()].size(); ++k)
+            {
+                map[all_map[j + i * aStage.scrolls().count()][k]] = k;
+            }
+            lDistanceMap[i * Parameter::MaxScrollCount + j] = all_map[j + i * aStage.scrolls().count()].size();
         }
     }
 
@@ -510,11 +596,25 @@ std::vector<int> create_list(const Stage& aStage)
 
 void setNextTarget(const Stage& aStage)
 {
-    if(distance_map == nullptr) distance_map = new double[Parameter::StageHeight * Parameter::StageWidth];
+    //if(distance_map == nullptr) distance_map = new double[Parameter::StageHeight * Parameter::StageWidth];
     map_initialize(distance_map, Parameter::StageWidth * Parameter::StageHeight);
-    target = aStage.scrolls()[target_list[list_index]].pos();
-    distance_map[static_cast<int>(target.x) + static_cast<int>(target.y) * Parameter::StageWidth] = 5.0;
-    create_distance_map(aStage, static_cast<int>(target.x), static_cast<int>(target.y));
+    if(list_index == 0)
+    {
+        target = aStage.scrolls()[target_list[0]].pos();
+        for(int i = 0; i < start_to_scroll[target_list[0]].size(); ++i)
+        {
+            distance_map[start_to_scroll[target_list[0]][i]] = i;
+        }
+    }
+    else
+    {
+    
+        target = aStage.scrolls()[target_list[list_index]].pos();
+        for(int i = 0; i < all_map[target_list[list_index] + target_list[list_index - 1] * aStage.scrolls().count()].size(); ++i)
+        {
+            distance_map[all_map[target_list[list_index] + target_list[list_index - 1] * aStage.scrolls().count()][i]] = i;
+        }
+    }
     ++list_index;
 }
 
@@ -559,7 +659,7 @@ Vector2 next_jump_point(const Stage& aStage)
         for(int j = -1; j < 2; ++j)
         {
             if(static_cast<int>(now_point.y) + j > 49 || static_cast<int>(now_point.y) + j < 0) continue;
-            //if(i == 0 && j == 0)break;
+            if(i == 0 && j == 0) continue;
             list.push_back(std::pair<double, Vector2>(distance_map[now_x + i + (now_y+j) * Parameter::StageWidth], Vector2(now_x + i, now_y + j)));
         }
     }
@@ -567,6 +667,7 @@ Vector2 next_jump_point(const Stage& aStage)
     {
         for(int j = min_y; j <= max_y; ++j)
         {
+            if(now_x == i && now_y == j) continue;
             if(is_reachble(i, j, power, now_point))
             {
                 list.push_back(std::pair<double, Vector2>(distance_map[i + j * Parameter::StageWidth], Vector2(i, j)));
@@ -586,6 +687,37 @@ Vector2 next_jump_point(const Stage& aStage)
     result = Vector2(result.x + 0.5, result.y + 0.5);
     return result;
 }
+
+void set_map(const Stage& aStage, int map_count)
+{
+    double tmp_map[Parameter::StageWidth * Parameter::StageHeight];
+    double tmp_map2[Parameter::StageHeight * Parameter::StageWidth];
+    
+    for(int i = 0; i < aStage.scrolls().count(); ++i)
+    {
+        Vector2 start_pos = aStage.rabbit().pos();
+        Vector2 end_pos = aStage.scrolls()[i].pos();
+        calc_distance(aStage, tmp_map, static_cast<int>(end_pos.x), static_cast<int>(end_pos.y));
+        calc_distance(aStage, tmp_map2, static_cast<int>(start_pos.x), static_cast<int>(start_pos.y));
+        start_to_scroll.push_back(aster_func(static_cast<int>(start_pos.x), static_cast<int>(start_pos.y), static_cast<int>(end_pos.x), static_cast<int>(end_pos.y), tmp_map2, tmp_map, aStage));
+    }
+    for(int i = 0; i < map_count; ++i)
+    {
+        for(int j = 0; j < map_count; ++j)
+        {
+            if(i == j)
+            {
+                all_map.push_back(std::vector<int>(0));
+                continue;
+            }
+            Vector2 start_pos = aStage.scrolls()[i].pos();
+            Vector2 end_pos = aStage.scrolls()[j].pos();
+            calc_distance(aStage, tmp_map, static_cast<int>(end_pos.x), static_cast<int>(end_pos.y));
+            calc_distance(aStage, tmp_map2, static_cast<int>(start_pos.x), static_cast<int>(start_pos.y));
+            all_map.push_back(aster_func(static_cast<int>(start_pos.x), static_cast<int>(start_pos.y), static_cast<int>(end_pos.x), static_cast<int>(end_pos.y), tmp_map2, tmp_map, aStage));
+        }
+    }
+}
  
 //------------------------------------------------------------------------------
 /// 各ステージ開始時に呼び出される処理
@@ -594,15 +726,13 @@ Vector2 next_jump_point(const Stage& aStage)
 void Answer::initialize(const Stage& aStage)
 {
     list_index = 0;
-    if(aStage.scrolls().count() == 1)
-    {
-        setTarget(aStage);
-    }
-    else
-    {
-        target_list = create_list(aStage);
-        setNextTarget(aStage);
-    }
+    all_map.clear();
+    start_to_scroll.clear();
+    distance_map = new double[Parameter::StageWidth * Parameter::StageHeight];
+    map_initialize(distance_map, Parameter::StageHeight * Parameter::StageWidth);
+    set_map(aStage, aStage.scrolls().count());
+    target_list = create_list(aStage);
+    setNextTarget(aStage);
     
     //target = setTarget(aStage);
 }
